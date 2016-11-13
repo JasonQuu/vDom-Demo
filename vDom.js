5d85a15880b8110cb65a98c29f76a7e5d55c2df0
Jason Qu <jasonkiddd@gmail.com>
2016-11-12 21:59:45 -0500

    update DFS walk method

--- vDom.js
+++ vDom.js
@@ -39,12 +39,26 @@ vElement.prototype.diff = function (oldTree, newTree) {
   return patches
 }
 
-// 对两棵树进行深度优先遍历
-function dfsWalk (oldNode, newNode, index, patches) {
-  // 对比oldNode和newNode的不同，记录下来
-  patches[index] = [...]
+function patch (node, patches) {
+  var walker = {index: 0}
+  dfsWalk(node, walker, patches)
+}
 
-  diffChildren(oldNode.children, newNode.children, index, patches)
+function dfsWalk (node, walker, patches) {
+  var currentPatches = patches[walker.index] // 从patches拿出当前节点的差异
+
+  var len = node.childNodes
+    ? node.childNodes.length
+    : 0
+  for (var i = 0; i < len; i++) { // 深度遍历子节点
+    var child = node.childNodes[i]
+    walker.index++
+    dfsWalk(child, walker, patches)
+  }
+
+  if (currentPatches) {
+    applyPatches(node, currentPatches) // 对当前节点进行DOM操作
+  }
 }
 
 // 遍历子节点
